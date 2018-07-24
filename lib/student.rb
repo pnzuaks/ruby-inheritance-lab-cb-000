2de404cafa8de9d4d11904c9181b017c69fe3af6
Patricia Nykaza <pnykaza@calstatela.edu>
2018-07-24 20:45:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,9 @@ require_relative "./user.rb"
 require 'pry'
 
 class Student < User
-  # attr_accessor :knowledge
   
   def initialize()
      @knowledge = []
-    # binding.pry
   end
 
   def learn(string)
